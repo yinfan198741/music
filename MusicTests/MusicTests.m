bccdff478d54ae28d294c4905a7fc269244eba8a
yinfan <yinfan@meituan.com>
2019-03-11 21:46:47 +0800

    Initial Commit

---
+++ MusicTests/MusicTests.m
@@ -0,0 +1,37 @@
+//
+//  MusicTests.m
+//  MusicTests
+//
+//  Created by fanyin on 2019/3/11.
+//  Copyright © 2019 fanyin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MusicTests : XCTestCase
+
+@end
+
+@implementation MusicTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
